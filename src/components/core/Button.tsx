488c9e71f1006cb54075ee2944e9fe065665add4
Barış Ertakuş <bariserta@gmail.com>
2022-05-04 14:08:36 +0300

    Add outlined and contained button

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -1,20 +1,27 @@
-import { TouchableOpacityProps } from "react-native";
+import { TouchableOpacityProps, Text, TextProps } from "react-native";
 import React, { useState } from "react";
 import styled from "styled-components/native";
 import Colors from "../../constants/Colors";
-import Text from "./Text";
 import rf from "../../utils/responsiveFont";
 import { hp } from "../../utils/responsiveScreen";
 
 type ButtonType = TouchableOpacityProps & {
   active: boolean;
+  variant?: "contained" | "outlined";
+};
+
+type TextType = TextProps & {
+  variant?: "contained" | "outlined";
+  active: boolean;
 };
 
 interface IButtonProps {
   title: string;
+  variant?: "contained" | "outlined";
+  onPress?: () => void;
 }
 
-const Button = ({ title }: IButtonProps) => {
+const Button = ({ title, variant = "contained", onPress }: IButtonProps) => {
   const [active, setActive] = useState(false);
 
   return (
@@ -23,8 +30,12 @@ const Button = ({ title }: IButtonProps) => {
       onPressOut={() => setActive(false)}
       active={active}
       activeOpacity={0.9}
+      variant={variant}
+      onPress={onPress}
     >
-      <StyledText title={title} bold />
+      <StyledText active={active} variant={variant}>
+        {title}
+      </StyledText>
     </Container>
   );
 };
@@ -34,13 +45,22 @@ export default Button;
 const Container = styled.TouchableOpacity<ButtonType>`
   margin-top: ${hp(2)}px;
   padding: ${hp(2)}px 0;
-  background-color: ${(props) =>
-    props.active ? Colors.primaryActiveBtn : Colors.primaryBtn};
+  background-color: ${({ active, variant }) =>
+    active
+      ? variant === "outlined"
+        ? Colors.primaryBtn
+        : Colors.primaryActiveBtn
+      : variant === "outlined"
+      ? Colors.white
+      : Colors.primaryBtn};
   border-radius: 10px;
+  ${(props) => props.variant === "outlined" && `border : 1px solid red;`}
 `;
 
-const StyledText = styled(Text)`
-  color: white;
+const StyledText = styled.Text<TextType>`
   font-size: ${rf(13)}px;
   text-align: center;
+  font-weight: bold;
+  color: ${({ variant, active }) =>
+    variant === "outlined" && !active ? Colors.primaryBtn : Colors.white};
 `;
