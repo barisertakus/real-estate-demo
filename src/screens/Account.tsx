f2441649a862d85702fc041858e9e727853d47e3
Barış Ertakuş <bariserta@gmail.com>
2022-05-04 06:06:04 +0300

    Update Account screen

--- src/screens/Account.tsx
+++ src/screens/Account.tsx
@@ -3,13 +3,14 @@ import { StyleSheet } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { TextField } from "rn-material-ui-textfield";
 import styled from "styled-components/native";
+import Button from "../components/core/Button";
 import Dropdown from "../components/core/Dropdown";
 import SafeLayout from "../components/core/SafeLayout";
 import Text from "../components/core/Text";
 import Colors from "../constants/Colors";
 import { changeLanguage, selectLanguage } from "../features/appSlice";
 import i18n from "../i18n";
-import { wp } from "../utils/responsiveScreen";
+import { hp, wp } from "../utils/responsiveScreen";
 
 const languages = [
   { label: "English", value: "en" },
@@ -28,25 +29,32 @@ const Account = () => {
   return (
     <SafeLayout>
       <Container>
-        <Header>
-          <Text title={i18n.t("account")} h2 />
-          <TextField
-            label={i18n.t("email")}
-            baseColor={Colors.primaryText}
-            inputContainerStyle={{ borderColor: Colors.primaryBorder }}
-          />
-          <TextField
-            label={i18n.t("password")}
-            baseColor={Colors.primaryText}
-            inputContainerStyle={{ borderColor: Colors.primaryBorder }}
-          />
-          <Dropdown
-            label={i18n.t("locale")}
-            data={languages}
-            handleChange={updateLanguage}
-          />
-        </Header>
-        <Bottom></Bottom>
+        <Content>
+          <Header>
+            <Text title={i18n.t("account")} h2 />
+          </Header>
+          <Inputs>
+            <TextField
+              label={i18n.t("email")}
+              baseColor={Colors.primaryText}
+              inputContainerStyle={{ borderColor: Colors.primaryBorder }}
+            />
+            <TextField
+              label={i18n.t("password")}
+              baseColor={Colors.primaryText}
+              inputContainerStyle={{ borderColor: Colors.primaryBorder }}
+            />
+            <DropdownWrapper>
+              <Dropdown
+                label={i18n.t("locale")}
+                data={languages}
+                handleChange={updateLanguage}
+              />
+            </DropdownWrapper>
+          </Inputs>
+          <Button title={i18n.t("signup")} />
+        </Content>
+        <Bottom />
       </Container>
     </SafeLayout>
   );
@@ -60,9 +68,20 @@ const Container = styled.View`
 `;
 
 const Header = styled.View`
-  flex: 0.6;
+  padding-top: ${hp(4)}px;
+`;
+
+const Inputs = styled.View``;
+
+const Content = styled.View`
+  flex: 0.65;
+  justify-content: space-between;
+`;
+
+const DropdownWrapper = styled.View`
+  margin-top: ${hp(2)}px;
 `;
 
 const Bottom = styled.View`
-  flex: 0.4;
+  flex: 0.35;
 `;
